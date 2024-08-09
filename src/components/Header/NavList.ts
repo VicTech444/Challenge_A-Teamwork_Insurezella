01457ac271f5a318eb8d1e72138c414d561bb3e6
VicTech444 <Victor_verq@outlook.com>
2024-08-09 11:26:48 -0500

    create header

---
+++ src/components/Header/NavList.ts
@@ -0,0 +1,33 @@
+export const NavList = [
+  {
+    name: "Home",
+    path: "/",
+    title: 'Homepage link'
+  },
+  {
+    name: "Find Insurance",
+    path: "/",
+    title: 'Find Insurance link',
+    collapseMenu: true,
+    collapseItems: [
+      {
+        name: "Collapse item one",
+        href: "/",
+      },
+      {
+        name: "Collapse item two",
+        href: "/",
+      }
+    ]
+  },
+  {
+    name: "Learn",
+    path: "/",
+    title: 'Learn link'
+  },
+  {
+    name: "About",
+    path: "/",
+    title: 'About link'
+  }
+];
