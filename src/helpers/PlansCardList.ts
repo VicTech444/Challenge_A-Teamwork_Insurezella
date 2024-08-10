b19c89ef08af6df91845a8a0b4317c475da19500
VicTech444 <Victor_verq@outlook.com>
2024-08-10 17:51:00 -0500

    add components inside header paragraph component and style it

---
+++ src/helpers/PlansCardList.ts
@@ -0,0 +1,23 @@
+export const PlansCardList = [
+    {
+        image: "consumer-protections.webp",
+        date: "15 Oct",
+        dateStyles: "bg-[#083791]",
+        title: "Reduced Out-of-Pocket Expenses",
+        description: "Comprehensive guides, expert advice, and simple tips to save on healthcare costs without sacrificing quality"
+    },
+    {
+        image: "consumer-protections.webp",
+        date: "20 Oct",
+        dateStyles: "bg-[#4c76f5]",
+        title: "Comprehensive Coverage Options",
+        description: "Comprehensive guides, expert advice, and simple tips to save on healthcare costs without sacrificing quality"
+    },
+    {
+        image: "consumer-protections.webp",
+        date: "22 Oct",
+        dateStyles: "bg-[#083791]",
+        title: "Keep your family coverede",
+        description: "Comprehensive guides, expert advice, and simple tips to save on healthcare costs without sacrificing quality"
+    }
+]
