826e4b3409a80cf7ffbb1928ab08dad8282552ec
VicTech444 <Victor_verq@outlook.com>
2024-08-11 10:45:24 -0500

    add carrousel for advantages section as react component

---
+++ src/components/Body/tablet-desktop/Carrousel.tsx
@@ -0,0 +1,51 @@
+// components/Carousel.jsx
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper/modules";
+import { PlansListDesktop } from "../../../helpers/PlansListDesktop";
+
+export default function Carousel() {
+  return (
+    <div className="w-full">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={2}
+        navigation={true}
+        pagination={{ clickable: true }}
+        modules={[Navigation, Pagination]}
+        breakpoints={{
+          1024: {
+            slidesPerView: 3,
+          },
+          1128: {
+            slidesPerView: 5,
+          },
+        }}
+      >
+        {PlansListDesktop.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <div className="flex justify-center mb-5">
+              <div className="flex flex-col rounded-md bg-white py-6">
+                <div className="flex flex-col items-center gap-y-2 px-9 py-10" style={{boxShadow: "0 6px 15px 2px #bdcdfc"}}>
+                  <img
+                    src={slide.href}
+                    alt="Logo of the image"
+                    title="Sample Image"
+                    draggable="false"
+                    className={`mx-auto h-auto w-16`}
+                    loading="eager"
+                  />
+                  <p className="mt-2 max-w-[100px] text-center text-lg font-bold text-[#2b2b47]">
+                    {slide.title}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
---
+++ src/helpers/PlansListDesktop.ts
@@ -0,0 +1,22 @@
+export const PlansListDesktop = [
+    {
+        href: '/health-insurance-icon.webp',
+        title: "Health Insurance",
+    },
+    {
+        href: '/medicare-icon.webp',
+        title: "Medicare Supplement",
+    },
+    {
+        href: '/medicare-advantage-icon.webp',
+        title: "Medicare Advantage",
+    },
+    {
+        href: '/life-insurance-icon.webp',
+        title: "Life Insurance",
+    },
+    {
+        href: '/obamacare-icon.webp',
+        title: "ACA obamacare",
+    },
+]
