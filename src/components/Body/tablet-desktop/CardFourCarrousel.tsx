aa8958e8b2df412537fdbc1113fb07ff7bc042f6
VicTech444 <Victor_verq@outlook.com>
2024-08-11 15:57:49 -0500

    create carrousel item component for card four

--- src/components/Body/tablet-desktop/CardFourCarrousel.tsx
+++ src/components/Body/tablet-desktop/CardFourCarrousel.tsx
@@ -4,6 +4,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination } from "swiper/modules";
+import { Fragment } from "react/jsx-runtime";
+import CarrouselItem from "./CardFourCarrouselItem";
 
 interface Props {
   image: string;
@@ -48,7 +50,7 @@ export default function CardFourCarrousel() {
     <>
       <div className="w-full lg:hidden">
         <Swiper
-          spaceBetween={50}
+          spaceBetween={0}
           slidesPerView={2}
           navigation={true}
           pagination={{ clickable: true }}
@@ -64,85 +66,14 @@ export default function CardFourCarrousel() {
         >
           {ItemList.map((slide, index) => (
             <SwiperSlide key={index}>
-              <div className="mb-8 flex justify-center px-20">
-                <div className="flex max-w-[380px] flex-col bg-white">
-                  <div className="flex justify-center">
-                    <img
-                      className="mx-auto flex aspect-video object-cover"
-                      src={`${slide.image}`}
-                      alt="Card Image"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-y-2 p-4 md:p-5">
-                    <div className="relative flex gap-x-6">
-                      <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-                        {slide.title}
-                      </h3>
-                    </div>
-                    <p className="mb-2 mt-1 text-[#9598ab]">
-                      {slide.description}
-                    </p>
-                    <p className="flex items-center gap-x-3 font-bold text-[#4e78f4]">
-                      {slide.actionCall}
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          fill="currentColor"
-                          d="M16.15 13H5q-.425 0-.712-.288T4 12t.288-.712T5 11h11.15L13.3 8.15q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L19.3 11.3q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.575 4.575q-.3.3-.712.288t-.713-.313q-.275-.3-.288-.7t.288-.7z"
-                        />
-                      </svg>
-                    </p>
-                  </div>
-                </div>
-              </div>
+              <CarrouselItem {...slide} index={index}/>
             </SwiperSlide>
           ))}
         </Swiper>
       </div>
       <div className="hidden grid-cols-2 grid-rows-2 lg:grid">
         {ItemList.map((slide, index) => (
-          <div className="mb-8 flex justify-center px-20" key={index}>
-            <div className="flex max-w-[380px] flex-col bg-white">
-              <div className="flex justify-center">
-                <img
-                  className="mx-auto flex aspect-video object-cover"
-                  src={`${slide.image}`}
-                  alt="Card Image"
-                />
-              </div>
-              <div className="flex flex-col gap-y-2 p-4 md:p-5 h-60">
-                <div className="relative flex gap-x-6">
-                  <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-                    {slide.title}
-                  </h3>
-                </div>
-                <p className="mb-2 mt-1 flex self-end text-[#9598ab]">
-                  {slide.description}
-                </p>
-
-                <div className="flex justify-self-end items-end my-auto">
-                  <p className="flex items-center gap-x-3 font-bold text-[#4e78f4]">
-                    {slide.actionCall}{" "}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M16.15 13H5q-.425 0-.712-.288T4 12t.288-.712T5 11h11.15L13.3 8.15q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L19.3 11.3q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.575 4.575q-.3.3-.712.288t-.713-.313q-.275-.3-.288-.7t.288-.7z"
-                      />
-                    </svg>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+         <CarrouselItem {...slide} index={index}/>
         ))}
       </div>
     </>
